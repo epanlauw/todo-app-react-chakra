cd05f16484b0bf46b0a07c8bbb11706d6e741f61
epanlauw <epanlauw6@gmail.com>
2024-05-13 17:45:49 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Text, VStack } from "@chakra-ui/react";
+import "./App.css";
+import { useState } from "react";
+import TodoList from "./components/TodoList";
+import AddTodo from "./components/AddTodo";
 
 function App() {
+  const todoList = [
+    { id: 1, text: "Buy eggs" },
+    { id: 2, text: "Walk the dog" },
+    { id: 3, text: "Watch a movie" },
+  ];
+
+  const [todos, setTodos] = useState(todoList);
+
+  const deleteTodo = (id) => {
+    const newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+
+    setTodos(newTodos);
+    console.log(newTodos);
+  };
+
+  const addTodo = (newTodo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const editTodo = (id, updateTodo) => {
+    const updateItem = todos.map((todo) => {
+      return todo.id === id ? updateTodo : todo;
+    });
+    setTodos(updateItem);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack p={5}>
+      <Text
+        bgGradient="linear(to-l, #7928CA,#FF0080)"
+        bgClip="text"
+        fontSize="6xl"
+        fontWeight="extrabold"
+      >
+        Todo App
+      </Text>
+      <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
+      <AddTodo addTodo={addTodo} />
+    </VStack>
   );
 }
 
